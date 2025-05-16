d0ae214cf613b58462cba0e1e594f5a82e449d4d
x <x.com>
2025-05-16 14:51:18 +0800

    ✨ feat: 优化

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,38 +1,59 @@
-// index.ts
-
 // 所有的数据
 const dataSource = [...Array(10000)].map((_, i) => i + 1);
 
 const PAGE_SIZE = 50;
 
+// 每个 item 的高度
 const ITEM_HEIGHT = 50;
 
-const BUFFER_SIZE = 5;
+// 可视区域前后缓冲区item个数
+const BUFFER_SIZE = 0;
+
+const throttle = (fn: () => void, delay = 200) => {
+  let timer: number | null = null;
+  return () => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      fn();
+      timer = null;
+    }, delay);
+  };
+};
 
 Page({
   data: {
-    // 已经加载的数据
-    loadedItems: [],
-    // 当前可见的数据
-    visibleItems: [],
+    // 已经加载的数据，作用：撑起高度，有滚动条
+    loadedItems: [] as number[],
+    // 应该渲染的数据
+    renderedItems: [] as { data: number; transform: string }[],
     // 是否显示加载更多按钮
     showMoreBtn: true,
-    // 每个 item 的高度
-    itemHeight: ITEM_HEIGHT,
-    // 滚动位置
-    scrollTop: 0
+    itemHeight: ITEM_HEIGHT
   },
 
-  onScroll(e) {
+  // 滚动位置
+  _scrollTop: 0,
+
+  throttledRender: null as any,
+
+  get totalHeight() {
+    return this.data.loadedItems.length * ITEM_HEIGHT;
+  },
+
+  onReady() {
+    this.getInitialData();
+  },
+
+  onLoad() {
+    this.throttledRender = throttle(() => {
+      this.getRenderedItems();
+    });
+  },
+
+  throttleScroll(e: WechatMiniprogram.CustomEvent) {
     const { scrollTop } = e.detail;
-    this.setData(
-      {
-        scrollTop
-      },
-      () => {
-        this.getVisibleItems();
-      }
-    );
+    this._scrollTop = scrollTop;
+    this.throttledRender();
   },
 
   getHeaderHeight(): Promise<number> {
@@ -45,6 +66,7 @@ Page({
       });
     });
   },
+
   getScrollViewHeight(): Promise<number> {
     return new Promise(resolve => {
       const query = wx.createSelectorQuery();
@@ -56,56 +78,68 @@ Page({
     });
   },
 
-  async getVisibleItems() {
-    const { scrollTop, loadedItems } = this.data;
-    const headerHeight = await this.getHeaderHeight();
-    const containerHeight = await this.getScrollViewHeight();
+  async getRenderedItems() {
+    const { loadedItems } = this.data;
+    const scrollTop = this._scrollTop;
+    const [headerHeight, containerHeight] = await Promise.all([
+      this.getHeaderHeight(),
+      this.getScrollViewHeight()
+    ]);
     console.log('containerHeight:', containerHeight);
     console.log('scrollTop:', scrollTop);
     console.log('headerHeight:', headerHeight);
 
-    const adjustedScrollTop = Math.max(0, scrollTop - headerHeight);
+    const windowInfo = wx.getWindowInfo();
+    const windowHeight = windowInfo.windowHeight;
+
+    // 展示内容视口高度
+    const viewHeight = Math.min(
+      containerHeight - headerHeight + scrollTop,
+      windowHeight
+    );
+
+    console.log('viewHeight', viewHeight);
+    const visibleItemCount = Math.ceil(viewHeight / ITEM_HEIGHT);
 
     if (scrollTop < headerHeight) {
-      const visibleCount = Math.ceil(
-        (containerHeight - headerHeight) / ITEM_HEIGHT
-      );
-      const _visibleItems = Array.from(
-        { length: Math.min(visibleCount + BUFFER_SIZE, loadedItems.length) },
-        (_, i) => ({
-          item: loadedItems[i],
-          index: i,
-          transform: `translateY(${i * ITEM_HEIGHT}px)`
-        })
-      );
+      const _items = loadedItems.slice(0, visibleItemCount + BUFFER_SIZE);
+      const _visibleItems = _items.map((item, index) => ({
+        data: item,
+        transform: `translateY(${index * ITEM_HEIGHT}px)`
+      }));
+
       this.setData(
         {
-          visibleItems: _visibleItems
+          renderedItems: _visibleItems
         },
         () => {
-          console.log('visibleItems1', this.data.visibleItems);
+          console.log('visibleItems1', this.data.renderedItems);
         }
       );
       return;
     }
 
+    const adjustedScrollTop = scrollTop - headerHeight;
+
     const startIndex = Math.max(
       0,
       Math.floor(adjustedScrollTop / ITEM_HEIGHT) - BUFFER_SIZE
     );
+
     const endIndex = Math.min(
       loadedItems.length - 1,
-      Math.ceil((adjustedScrollTop + containerHeight) / ITEM_HEIGHT) +
-        BUFFER_SIZE
+      startIndex + visibleItemCount + BUFFER_SIZE
     );
+
+    console.log('startIndex', startIndex);
+    console.log('endIndex', endIndex);
 
     const _visibleItems = Array.from(
       { length: endIndex - startIndex + 1 },
       (_, i) => {
         const index = startIndex + i;
         return {
-          item: loadedItems[index],
-          index,
+          data: loadedItems[index],
           transform: `translateY(${index * ITEM_HEIGHT}px)`
         };
       }
@@ -113,10 +147,10 @@ Page({
 
     this.setData(
       {
-        visibleItems: _visibleItems
+        renderedItems: _visibleItems
       },
       () => {
-        console.log('visibleItems2', this.data.visibleItems);
+        console.log('visibleItems2', this.data.renderedItems);
       }
     );
   },
@@ -139,6 +173,12 @@ Page({
     );
   },
 
+  loadMore() {
+    console.log('11', 11);
+    // this.handleLoadMore();
+    // this.getRenderedItems();
+  },
+
   getInitialData() {
     const initData = dataSource.slice(0, PAGE_SIZE);
     this.setData(
@@ -146,12 +186,8 @@ Page({
         loadedItems: initData
       },
       () => {
-        this.getVisibleItems();
+        this.getRenderedItems();
       }
     );
-  },
-
-  onReady() {
-    this.getInitialData();
   }
 });
